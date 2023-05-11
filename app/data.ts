f5d229835d756a962e7a0f5ab0b49a2ff714d593
Duminda Kodagoda <duminda.g.k@gmail.com>
2023-05-11 21:09:52 +0530

    init

---
+++ app/data.ts
@@ -0,0 +1,147 @@
+export type TTimelineData = {
+  adSlotCode: string;
+  adSlotName: string;
+  adSlotGroupCode: null | string;
+  idAdSlotGroup: number;
+  maxActiveSlotsInGroup: number;
+  bookingCode: string;
+  statusCode: string;
+  pageCode: string;
+  pageName: string;
+  widgetCode: string;
+  widgetType: string;
+  campaignCode: string;
+  campaignName: string;
+  assetLinkType: null | string;
+  assetLinkValue: null | string;
+  startDate: string;
+  endDate: string;
+  isActive: boolean;
+  imageEn: null | string;
+  imageAr: null | string;
+  campaignTags: [];
+  campaignBudgetUsd: null | string;
+};
+
+export const chartData: TTimelineData[] = [
+  {
+    adSlotCode: "widget_3--1",
+    adSlotName: "HB - Position 1",
+    adSlotGroupCode: null,
+    idAdSlotGroup: 1,
+    maxActiveSlotsInGroup: 1,
+    bookingCode: "B4LQPB4VJ",
+    statusCode: "confirmed",
+    pageCode: "home_app_mobile_ae",
+    pageName: "UAE Home Page",
+    widgetCode: "widget_3",
+    widgetType: "bannerSlider",
+    campaignCode: "CWWUSWKW9",
+    campaignName: "Test_Cam",
+    assetLinkType: "tag",
+    assetLinkValue: "Test",
+    startDate: "2023-05-04T03:53:19",
+    endDate: "2023-05-19T03:53:19",
+    isActive: false,
+    imageEn: null,
+    imageAr: null,
+    campaignTags: [],
+    campaignBudgetUsd: null,
+  },
+  {
+    adSlotCode: "widget_3--1",
+    adSlotName: "HB - Position 1",
+    adSlotGroupCode: null,
+    idAdSlotGroup: 1,
+    maxActiveSlotsInGroup: 1,
+    bookingCode: "BSIQKAI50",
+    statusCode: "cancelled",
+    pageCode: "home_app_mobile_ae",
+    pageName: "UAE Home Page",
+    widgetCode: "widget_3",
+    widgetType: "bannerSlider",
+    campaignCode: "CQZTRGANV",
+    campaignName: "test123",
+    assetLinkType: null,
+    assetLinkValue: null,
+    startDate: "2023-05-02T09:25:08",
+    endDate: "2023-05-06T09:25:08",
+    isActive: false,
+    imageEn: null,
+    imageAr: null,
+    campaignTags: [],
+    campaignBudgetUsd: null,
+  },
+  {
+    adSlotCode: "widget_3--1",
+    adSlotName: "HB - Position 1",
+    adSlotGroupCode: null,
+    idAdSlotGroup: 1,
+    maxActiveSlotsInGroup: 1,
+    bookingCode: "BFMNFTQY9",
+    statusCode: "cancelled",
+    pageCode: "home_app_mobile_ae",
+    pageName: "UAE Home Page",
+    widgetCode: "widget_3",
+    widgetType: "bannerSlider",
+    campaignCode: "CQZTRGANV",
+    campaignName: "test123",
+    assetLinkType: null,
+    assetLinkValue: null,
+    startDate: "2023-05-02T09:25:08",
+    endDate: "2023-05-06T09:25:08",
+    isActive: false,
+    imageEn: null,
+    imageAr: null,
+    campaignTags: [],
+    campaignBudgetUsd: null,
+  },
+  {
+    adSlotCode: "widget_3--1",
+    adSlotName: "HB - Position 1",
+    adSlotGroupCode: null,
+    idAdSlotGroup: 1,
+    maxActiveSlotsInGroup: 1,
+    bookingCode: "BM5XD6X6C",
+    statusCode: "cancelled",
+    pageCode: "home_app_mobile_ae",
+    pageName: "UAE Home Page",
+    widgetCode: "widget_3",
+    widgetType: "bannerSlider",
+    campaignCode: "CWWUSWKW9",
+    campaignName: "Test_Cam",
+    assetLinkType: null,
+    assetLinkValue: null,
+    startDate: "2023-05-06T09:51:57",
+    endDate: "2023-06-30T09:51:57",
+    isActive: false,
+    imageEn: null,
+    imageAr: null,
+    campaignTags: [],
+    campaignBudgetUsd: null,
+  },
+  {
+    adSlotCode: "widget_3--1",
+    adSlotName: "HB - Position 1",
+    adSlotGroupCode: null,
+    idAdSlotGroup: 1,
+    maxActiveSlotsInGroup: 1,
+    bookingCode: "BF3IKOI91",
+    statusCode: "tentative",
+    pageCode: "home_app_mobile_ae",
+    pageName: "UAE Home Page",
+    widgetCode: "widget_3",
+    widgetType: "bannerSlider",
+    campaignCode: "CCNFDTLBX",
+    campaignName: "tetststs",
+    assetLinkType: "brand",
+    assetLinkValue: null,
+    startDate: "2023-05-04T07:03:28",
+    endDate: "2023-05-11T07:03:28",
+    isActive: false,
+    imageEn: null,
+    imageAr: null,
+    campaignTags: [],
+    campaignBudgetUsd: null,
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,192 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export default function Home() {
+  const numberOfDays = 31;
+  const fixWidth = 60;
+  const fullWidth = numberOfDays * fixWidth;
+  const dates = new Array(31).fill(0).map((_, i) => i + 1);
+  const barHeight = 30;
+  const barMargins = 40;
+
+  const monthData = {
+    "widget_3--1": [
+      {
+        adSlotName: "HB - Position 1",
+        startDate: "2023-05-04T03:53:19",
+        endDate: "2023-05-19T03:53:19",
+        start: 4,
+        width: 5,
+        background: "#F0F2F7",
+        color: "#2F53AE",
+      },
+      {
+        adSlotName: "HB - Position 1",
+        startDate: "2023-05-04T03:53:19",
+        endDate: "2023-05-19T03:53:19",
+        start: 6,
+        width: 8,
+        background: "#FFEBC8",
+        color: "#8F671F",
+      },
+      {
+        adSlotName: "HB - Position 1",
+        startDate: "2023-05-04T03:53:19",
+        endDate: "2023-05-19T03:53:19",
+        start: 6,
+        width: 6,
+        background: "#EFFFE8",
+        color: "#54AB2E",
+      },
+    ],
+    "widget_3--2": [
+      {
+        adSlotName: "HB - Position 1",
+        startDate: "2023-05-04T03:53:19",
+        endDate: "2023-05-19T03:53:19",
+        start: 6,
+        width: 3,
+        background: "#F0F2F7",
+        color: "#2F53AE",
+      },
+      {
+        adSlotName: "HB - Position 1",
+        startDate: "2023-05-04T03:53:19",
+        endDate: "2023-05-19T03:53:19",
+        start: 1,
+        width: 5,
+        background: "#F0F2F7",
+        color: "#2F53AE",
+      },
+      {
+        adSlotName: "HB - Position 1",
+        startDate: "2023-05-04T03:53:19",
+        endDate: "2023-05-19T03:53:19",
+        start: 6,
+        width: 4,
+        background: "#F0F2F7",
+        color: "#2F53AE",
+      },
+    ],
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.left}>
+        <div className={styles.leftTop}></div>
+        <div className={styles.leftSecond}></div>
+        <div className={styles.leftThird}>
+          {Object.entries(monthData).map(([key, value]) => (
+            <div
+              className={styles.leftSecondGroup}
+              key={`group-${key}`}
+              style={{ height: `${barHeight * value.length + barMargins}px` }}
+            >
+              {key}
+            </div>
+          ))}
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.right}>
+        <div className={styles.top}>
+          <div className={styles.topLeft}>January, 2023</div>
+          <div className={styles.topRight}>
+            <div className={styles.topRightItem}>
+              <div
+                className={styles.topRightItemColor}
+                style={{ background: "#E9EFFF", borderColor: "#2F53AE" }}
+              ></div>
+              <div
+                className={styles.topRightItemDescription}
+                style={{ color: "#2F53AE" }}
+              >
+                Currently Booking
+              </div>
+            </div>
+            <div className={styles.topRightItem}>
+              <div
+                className={styles.topRightItemColor}
+                style={{ background: "#FFEBC8", borderColor: "#8F671F" }}
+              ></div>
+              <div
+                className={styles.topRightItemDescription}
+                style={{ color: "#8F671F" }}
+              >
+                Tentative
+              </div>
+            </div>
+            <div className={styles.topRightItem}>
+              <div
+                className={styles.topRightItemColor}
+                style={{ background: "#EFFFE8", borderColor: "#54AB2E" }}
+              ></div>
+              <div
+                className={styles.topRightItemDescription}
+                style={{ color: "#54AB2E" }}
+              >
+                Confirmed
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.calendarCtr}>
+          <div className={styles.grid} style={{ width: `${fullWidth}px` }}>
+            {dates.map((_, i) => (
+              <div
+                className={styles.gridLines}
+                key={`lines-${i + 1}`}
+                style={{ left: `${fixWidth * i}px` }}
+              ></div>
+            ))}
+            {/* {Object.entries(monthData).map(([key, value]) => (
+              <>
+                {value.map((item, i) => (
+                  <div
+                    className={styles.horizontalLines}
+                    key={`lines-${i + 1}`}
+                    style={{
+                      top: `${barHeight * i + barMargins}px`,
+                    }}
+                  ></div>
+                ))}
+              </>
+            ))} */}
+          </div>
+          <div className={styles.calendar} style={{ width: `${fullWidth}px` }}>
+            {dates.map((date, i) => (
+              <div
+                className={styles.dateBox}
+                key={`date-${i + 1}`}
+                style={{ width: `${fixWidth}px`, height: `${fixWidth}px` }}
+              >
+                {date}
+              </div>
+            ))}
+          </div>
+          {Object.entries(monthData).map(([key, value]) => (
+            <div
+              className={styles.calenderGroup}
+              key={`group-${key}`}
+              style={{ height: `${barHeight * value.length + barMargins}px` }}
+            >
+              {value.map((item, i) => (
+                <div
+                  className={styles.calenderItem}
+                  key={`group-${i}`}
+                  style={{
+                    height: `${barHeight}px`,
+                    left: `${fixWidth * item.start}px`,
+                    width: `${fixWidth * item.width}px`,
+                    background: `${item.background}`,
+                    color: `${item.color}`,
+                  }}
+                >
+                  {item.adSlotName}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </main>
-  )
+  );
 }
